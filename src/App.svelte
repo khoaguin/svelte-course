7ba463e50b17c655d6acbbe03bbd99f0c55684a3
khoaguin <dkn.work@protonmail.com>
2023-05-14 18:13:05 +0700

    start coding the TodoList component

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,8 @@
 <script>
-	import Button from './lib/Button.svelte';
-	import DiCodeigniter from 'svelte-icons/di/DiCodeigniter.svelte';
-	import DiAtom from 'svelte-icons/di/DiAtom.svelte';
+	import TodoList from './lib/TodoList.svelte';
 </script>
 
-<Button
-	on:click|once={() => alert('button clicked')}
-	let:isLeftHovered
-	size="large"
-	shadow
-	bgColor="#FF5E0E"
-	textColor="white"
-	disabled
->
-	<div style:width="20px" slot="leftContent">
-		{#if isLeftHovered}
-			<DiCodeigniter />
-		{:else}
-			<DiAtom />
-		{/if}
-	</div>
-	Button Text
-</Button>
+<TodoList />
 
 <style>
 </style>
