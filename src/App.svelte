2fdf872f822db66bf68f5ffb03797d1e1375e0f2
khoaguin <dkn.work@protonmail.com>
2023-05-14 17:40:58 +0700

    done section 3

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,27 @@
 <script>
 	import Button from './lib/Button.svelte';
+	import DiCodeigniter from 'svelte-icons/di/DiCodeigniter.svelte';
+	import DiAtom from 'svelte-icons/di/DiAtom.svelte';
 </script>
 
-<Button size="large" shadow bgColor="#FF5E0E" textColor="white">My Button</Button>
+<Button
+	on:click|once={() => alert('button clicked')}
+	let:isLeftHovered
+	size="large"
+	shadow
+	bgColor="#FF5E0E"
+	textColor="white"
+	disabled
+>
+	<div style:width="20px" slot="leftContent">
+		{#if isLeftHovered}
+			<DiCodeigniter />
+		{:else}
+			<DiAtom />
+		{/if}
+	</div>
+	Button Text
+</Button>
 
 <style>
 </style>
--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -3,20 +3,35 @@
 	export let shadow = false;
 	export let bgColor = 'black';
 	export let textColor = 'white';
+	let isLeftHovered;
 </script>
 
 <button
+	on:click
 	style:background-color={bgColor}
 	style:color={textColor}
 	class:size-lg={size == 'large'}
 	class:size-sm={size == 'small'}
+	class:has-left={$$slots.leftContent}
 	class:shadow
+	{...$$restProps}
 >
-	<slot>Fallback</slot>
+	{#if $$slots.leftContent}
+		<div
+			class="left-content"
+			on:mouseenter={() => (isLeftHovered = true)}
+			on:mouseleave={() => (isLeftHovered = false)}
+		>
+			<slot name="leftContent" />
+		</div>
+	{/if}
+	<slot {isLeftHovered}>Fallback</slot>
 </button>
 
 <style lang="scss">
 	button {
+		display: flex;
+		align-items: center;
 		background-color: var(--buttonBgColor);
 		border: none;
 		color: var(--buttonTextColor);
@@ -25,6 +40,13 @@
 		padding: 15px 20px;
 		border-radius: 5px;
 		cursor: pointer;
+		.left-content {
+			margin-right: 10px;
+		}
+		&:disabled {
+			opacity: 0.6;
+			cursor: not-allowed;
+		}
 		&:hover {
 			background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
 		}
