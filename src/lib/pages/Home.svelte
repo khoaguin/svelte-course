2651b3b6f6cb2c66045d8d33bab29efac15158c2
khoaguin <dkn.work@protonmail.com>
2023-06-28 20:56:59 +0700

    auto subscription to store and store binding

---
+++ src/lib/pages/Home.svelte
@@ -0,0 +1,7 @@
+<script>
+	import settings from '../stores/settings';
+</script>
+
+<h2>Home</h2>
+
+{$settings.colorScheme}
---
+++ src/lib/pages/Settings.svelte
@@ -0,0 +1,24 @@
+<script>
+	import settings from '../stores/settings';
+</script>
+
+<h2>Settings</h2>
+
+<!-- a button to update store's value -->
+<button
+	on:click={() => {
+		// if the colorScheme is dark, set it to light, otherwise set it to dark
+		$settings.colorScheme = $settings.colorScheme == 'dark' ? 'light' : 'dark';
+	}}>Toggle Color Scheme</button
+>
+
+<!-- we can also bind inputs to store values -->
+<label>
+	<input type="radio" bind:group={$settings.colorScheme} name="colorScheme" value="dark" /> Dark
+</label>
+<label>
+	<input type="radio" bind:group={$settings.colorScheme} name="colorScheme" value="light" /> Light
+</label>
+<div>
+	{$settings.colorScheme}
+</div>
