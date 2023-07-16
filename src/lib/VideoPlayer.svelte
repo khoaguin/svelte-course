07c4b3e6b74720c95d4d3befe1d7c83471b061d3
khoaguin <dkn.work@protonmail.com>
2023-07-16 18:46:51 +0700

    Creating a Simple Video Player Component & Binding Media Information

---
+++ src/lib/VideoPlayer.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let src = undefined;
+
+	let paused = true;
+	let currentTime = 0;
+	let volume = 0;
+</script>
+
+<video class:playing={!paused} {src} controls muted bind:paused bind:volume bind:currentTime />
+
+<button
+	on:click={() => {
+		paused = !paused;
+	}}>{paused ? 'Play' : 'Pause'}</button
+>
+<p>current time: {currentTime}</p>
+<p>volume: {volume}</p>
+
+<style>
+	video.playing {
+		outline: 4px solid red;
+	}
+</style>
--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,46 +1,7 @@
 <script>
-	import { Stage, Layer, Rect } from '../konva';
-	let showCanvas = true;
-	let x = 20;
-
-	let rect1, rect2;
+	import VideoPlayer from '../VideoPlayer.svelte';
 </script>
 
-<input type="checkbox" bind:checked={showCanvas} />
-<input type="range" bind:value={x} min={0} max={300} />
-<button
-	on:click={() => {
-		console.log('1st rect', rect1.rect.getAttrs());
-		console.log('2nd rect', rect2.rect.getAttrs());
-	}}>Get Rect Info</button
->
-
-{#if showCanvas}
-	<Stage width={300} height={400}>
-		<Layer
-			draggable
-			on:dragmove={(e) => {
-				console.log(e.detail);
-			}}
-			on:click={(e) => {
-				console.log(e.detail);
-			}}
-		>
-			<Rect bind:this={rect1} {x} y={20} fill="blue" width={100} height={100} />
-			<Rect x={60} y={60} fill="red" width={100} height={100} />
-		</Layer>
-		<Layer {x}>
-			<Rect
-				bind:this={rect2}
-				on:click={() => console.log('green clicked')}
-				on:mousedown={() => console.log('green mousedown')}
-				x={100}
-				y={100}
-				fill="green"
-				width={100}
-				height={100}
-				stroke="white"
-			/>
-		</Layer>
-	</Stage>
-{/if}
+<VideoPlayer src="mov_bbb.mp4" />
+<VideoPlayer src="mov_bbb.mp4" />
+<VideoPlayer src="mov_bbb.mp4" />
